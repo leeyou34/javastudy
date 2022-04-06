af8ffc1d1071b0a841c30ad436dc6583ad6226d1
Thom <81919273+leeyou34@users.noreply.github.com>
2022-04-06 19:15:09 +0900

    Create jdoodlepractice.java

---
+++ jdoodlepractice.java
@@ -0,0 +1,11 @@
+// based on course, i pasted the course materials
+
+public class MyClass {
+    public static void main(String args[]) {
+      int x=10;
+      int y=25;
+      int z=x+y;
+
+      System.out.println("Sum of x+y = " + z);
+    }
+}
