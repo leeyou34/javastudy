fe0248975ebf38761a2433fbb346ab1991c759ed
yh.thomas.lee <yh.thomas.lee@gmail.com>
2022-04-11 17:22:14 +0900

    java update

--- javaegoing/java1/HelloWorldRaspberryPi/src/HelloWorldRaspberryPi.java
+++ javaegoing/java1/HelloWorldRaspberryPi/src/HelloWorldRaspberryPi.java
@@ -5,14 +5,13 @@ import com.pi4j.io.gpio.GpioPinDigitalOutput;
 
 public class HelloWorldRaspberryPi {
 	
-	public static void main(String[] args) throws InterruptedException {
-		
-		final GpioController gpio = GpioFactory.getInstance();
-		final GpioPinDigitalOutput pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01,"PinLED", PinState.LOW);
-		
-		final int SHORT_INTERVAL=200;
-		final int LONG_INTERVAL= SHORT_INTERVAL * 3;
-		final int LETTER_INTERVAL = SHORT_INTERVAL * 7;
+		public static void main(String[] args) throws InterruptedException {
+
+			final GpioController gpio = GpioFactory.getInstance();
+			final GpioPinDigitalOutput pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "PinLED", PinState.LOW);
+			final int SHORT_INTERVAL = 200;
+			final int LONG_INTERVAL = SHORT_INTERVAL * 3;
+			final int LETTER_INTERVAL = SHORT_INTERVAL * 7;
 		
 		while(true) {
 			//H
