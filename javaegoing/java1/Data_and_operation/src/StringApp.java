57d127bffa399ce8d1b9833b028cfd2a05be43e0
yh.thomas.lee <yh.thomas.lee@gmail.com>
2022-04-11 19:24:04 +0900

    StringApp

---
+++ javaegoing/java1/Data_and_operation/src/StringApp.java
@@ -0,0 +1,21 @@
+
+public class StringApp {
+
+	public static void main(String[] args) {
+
+
+		// Character VS String
+		
+		System.out.println("Hello World");
+		System.out.println("Hello" + "World");
+
+		System.out.println('H');
+		System.out.println("H");
+		
+		System.out.println("Hello " + "World");
+		
+		System.out.println("Hello \nWorld");
+		System.out.println("Hello \"World\"");
+	}
+
+}
