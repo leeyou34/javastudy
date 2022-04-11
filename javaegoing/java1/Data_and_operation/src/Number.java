168fedf0a8ca80b062e86e3f8e02399ff176c94d
yh.thomas.lee <yh.thomas.lee@gmail.com>
2022-04-11 19:07:11 +0900

    number section

---
+++ javaegoing/java1/Data_and_operation/src/Number.java
@@ -0,0 +1,16 @@
+
+public class Number {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(6 + 2); // 8
+		System.out.println(6 - 2); // 4
+		System.out.println(6 * 2); // 12
+		System.out.println(6 / 2); // 3
+		
+		System.out.println(Math.PI);
+		System.out.println(Math.floor(Math.PI));
+		System.out.println(Math.ceil(Math.PI));
+		
+	}
+}
