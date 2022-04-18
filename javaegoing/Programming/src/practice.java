fbfd124b5283931c505cb0ef07f1fd0837c34112
yh.thomas.lee <yh.thomas.lee@gmail.com>
2022-04-18 13:28:38 +0900

    changed the practice part

--- javaegoing/Programming/src/practice.java
+++ javaegoing/Programming/src/practice.java
@@ -4,7 +4,8 @@ public class practice {
 		public static int a = 100;
 		
 		public static void main(String[] args) {
-			public int b = 10;
+			//public int b = 10;
+			final int b =10;
 			
 			final double pi =3.14;
 			
