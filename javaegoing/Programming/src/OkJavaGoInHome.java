c28cfb54a609b85e7a5d9e413aa5820a103ae7b5
yh.thomas.lee <yh.thomas.lee@gmail.com>
2022-04-18 10:18:09 +0900

    ClassApp

--- javaegoing/Programming/src/OkJavaGoInHome.java
+++ javaegoing/Programming/src/OkJavaGoInHome.java
@@ -1,3 +1,5 @@
+import javax.swing.JOptionPane;
+
 import org.opentutorials.iot.Elevator;
 import org.opentutorials.iot.Security;
 import org.opentutorials.iot.Lighting;
@@ -7,7 +9,10 @@ public class OkJavaGoInHome {
 
 	public static void main(String[] args) {
 
-
+		//String id = JOptionPane.showInputDialog("Enter a ID");
+		String id = args [0];
+		String bright = JOptionPane.showInputDialog("Enter a Bright level");
+		
 		// Elevator call
 		Elevator myElevator = new Elevator("JAVA APT 507");
 		myElevator.callForUp(1);
@@ -22,6 +27,7 @@ public class OkJavaGoInHome {
 		
 		Lighting floorLamp = new Lighting("JAVA APT 507 / floorLamp");
 		floorLamp.on();
+		
 
 	}
 
