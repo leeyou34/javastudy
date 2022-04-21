ea791b578eca14b698341109ef7e0a21848c2dec
yh.thomas.lee <yh.thomas.lee@gmail.com>
2022-04-21 09:53:52 +0900

    conditional statement

---
+++ javaegoing/java-flow-control/src/AuthApp.java
@@ -0,0 +1,24 @@
+
+public class AuthApp {
+
+	public static void main(String[] args) {
+
+		String id = "egoing";
+		String inputId = args [0];
+		
+		String pass = "1111";
+		String inputPass = args[1];
+		
+		System.out.println("Hi");
+		
+		if(inputId == id) {
+			if(inputPass.equals(pass)) {
+			System.out.println("Master!");
+		} else {
+			System.out.println("wrong password?");
+		}
+	} else {
+		System.out.println("who are u?")
+	}
+
+}
---
+++ javaegoing/java-flow-control/src/IfApp.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class IfApp {
+
+	public static void main(String[] args) {
+
+//		System.out.println("a");
+//		if(true) {
+//			System.out.println(1);
+//		} else {
+//			if(true) {
+//				System.out.println(2);
+//			} else {
+//				System.out.println(3);
+//			}
+//		}
+
+		Scanner sc = new Scanner(System.in);
+		
+		if(false) {
+			System.out.println(1);
+		} else if(true) {
+			System.out.println(2);
+		} else {
+			System.out.println(3);
+		}
+		
+		System.out.println("b");
+
+	}
+}
+
