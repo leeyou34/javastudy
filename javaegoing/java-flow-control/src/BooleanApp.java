055cd99ee4c1085e9bff3a262b7511f44d6cfef4
yh.thomas.lee <yh.thomas.lee@gmail.com>
2022-04-21 09:14:53 +0900

    comparison operator

---
+++ javaegoing/java-flow-control/src/BooleanApp.java
@@ -0,0 +1,18 @@
+
+public class BooleanApp {
+
+	public static void main(String[] args) {
+
+
+		System.out.println("One");
+		System.out.println(1);
+		
+		System.out.println(true);
+		System.out.println(false);
+		
+		String foo = "Hello World";
+		
+		System.out.println(foo);
+	}
+
+}
