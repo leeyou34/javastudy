3b8dba3dd9504b84283e656c24ea487982524f0c
yh.thomas.lee <yh.thomas.lee@gmail.com>
2022-04-21 11:24:17 +0900

    AuthApp4

---
+++ javaegoing/java-flow-control/src/AuthApp4.java
@@ -0,0 +1,36 @@
+
+public class AuthApp4 {
+	
+	public static void main (String [] args) {
+		
+		String[][] users = {
+			{"egoing", "1111"},
+			{"youbin", "2222"},
+			{"thomas", "3333"}
+		};
+		
+		String inputId = args[0];
+		String inputPass = args[1];
+		
+		boolean isLogined = false;
+		
+		for(int i=0; i<users.length; i++) {
+			 String current = users[i];
+			
+			if(
+				current[0].equals(inputId) && 
+				current[1].equals(inputPass)
+			  ) {
+				isLogined = true;
+				break;
+			}
+		}
+		
+		System.out.println("Hi,");
+		if(isLogined) {
+			System.out.println("Master!!!");
+		} else {
+			System.out.println("who is this?");
+		}
+	}
+}
