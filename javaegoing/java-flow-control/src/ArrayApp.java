e2b86da1672cd44beb48aae8156d808787265ec5
yh.thomas.lee <yh.thomas.lee@gmail.com>
2022-04-21 10:46:52 +0900

    array part

---
+++ javaegoing/java-flow-control/src/ArrayApp.java
@@ -0,0 +1,19 @@
+
+public class ArrayApp {
+
+	public static void main(String[] args) {
+		
+		// egoing, jinhuck, youbin
+		// String users = "egoing, jinhuck, youbin";
+		
+		String[] users = new String [3];
+		
+		users[0] = "egoing";
+		users[1] = "jinhuck";
+		users[2] = "youbin";
+		
+		System.out.println(users[1]);
+
+	}
+
+}
