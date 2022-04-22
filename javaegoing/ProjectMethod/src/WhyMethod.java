a6881305811c2ec2f083c0390a80f203b34cd1bd
yh.thomas.lee <yh.thomas.lee@gmail.com>
2022-04-22 09:48:37 +0900

    using arguments

--- javaegoing/ProjectMethod/src/WhyMethod.java
+++ javaegoing/ProjectMethod/src/WhyMethod.java
@@ -1,3 +1,4 @@
+//import java.util.Scanner;
 
 public class WhyMethod {
 	
@@ -7,19 +8,38 @@ public class WhyMethod {
 		System.out.println("A");
 		
 	}
-
+	
+	public static void printTwoTimesB() {
+		System.out.println("-");
+		System.out.println("B");
+		System.out.println("B");
+	}
+	
+	
+	public static void printTwoTimes(String text, String delimiter) {
+		System.out.println(delimiter);
+		System.out.println(text);
+		System.out.println(text);
+	}
 	public static void main(String [] args) {
-			
-		//100000000
-		printTwoTimesA();
-		
-		//100000000
-		printTwoTimesA();
-		
-		//100000000
-		printTwoTimesA();
-				
-		
+//			
+//		//100000000
+//		printTwoTimesA();
+//		
+//		//100000000
+//		printTwoTimesA();
+//		
+//		//100000000
+//		printTwoTimesA();
+//		
+//		//print differently.
+//		printTwoTimesB();
+//		
+		//Scanner sc = new Scanner(System.in);
+		printTwoTimes("a", "-");
+		printTwoTimes("b", "!");
+		printTwoTimes("c", "@");
+		printTwoTimes("d", "$");
 	}
 
 }
