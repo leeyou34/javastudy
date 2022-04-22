29c9d10e073a1e1516263c3fb0a473ac65a5f698
yh.thomas.lee <yh.thomas.lee@gmail.com>
2022-04-22 09:22:13 +0900

    method

---
+++ javaegoing/ProjectMethod/src/WhyMethod.java
@@ -0,0 +1,25 @@
+
+public class WhyMethod {
+	
+	public static void printTwoTimesA() {
+		System.out.println("-");
+		System.out.println("A");
+		System.out.println("A");
+		
+	}
+
+	public static void main(String [] args) {
+			
+		//100000000
+		printTwoTimesA();
+		
+		//100000000
+		printTwoTimesA();
+		
+		//100000000
+		printTwoTimesA();
+				
+		
+	}
+
+}
