e944c7ee19a9c38373418ef3d9e97acf788c6d25
yh.thomas.lee <yh.thomas.lee@gmail.com>
2022-04-25 10:18:16 +0900

    making class

--- javaegoing/ProjectOOP/src/MyOOP.java
+++ javaegoing/ProjectOOP/src/MyOOP.java
@@ -1,39 +1,18 @@
-
 public class MyOOP {
 	
 	public static void main(String[] args) {
-		delimiter = "-----------";
+		Print.delimiter = "-----------";
+		Print.A();
+		Print.B();
 		
 		//..
-		PrintA("");
-		printB("");
-		
-		//..
-		delimiter = "**********";
-		printB("");
-		PrintA("");
+		Print.delimiter = "**********";
+		Print.B();
+		Print.A();
 		
 		//..
 		
 
 	}
-	
-	public static String delimiter= "";
-	
-	public static void printB(String delimiter) {
-		System.out.println(delimiter);
-		System.out.println("B");
-		System.out.println("B");
-		System.out.println("B");
-		System.out.println("AB");
-	}
-
-	public static void PrintA(String delimiter) {
-		System.out.println(delimiter);
-		System.out.println("A");
-		System.out.println("A");
-		System.out.println("A");
-		System.out.println("A");
-	}
-
 }
+
---
+++ javaegoing/ProjectOOP/src/Print.java
@@ -0,0 +1,21 @@
+
+class Print {	
+	public static String delimiter= "";
+	
+	public static void B() {
+		System.out.println(delimiter);
+		System.out.println("B");
+		System.out.println("B");
+		System.out.println("B");
+		System.out.println("AB");
+	}
+
+	public static void A() {
+		System.out.println(delimiter);
+		System.out.println("A");
+		System.out.println("A");
+		System.out.println("A");
+		System.out.println("A");
+	}
+
+}
