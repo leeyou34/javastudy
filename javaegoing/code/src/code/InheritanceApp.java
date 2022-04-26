5fc20c0ee3fbe8a4d512ac6d4f0e5f8bfac90bda
yh.thomas.lee <yh.thomas.lee@gmail.com>
2022-04-26 14:05:02 +0900

    상속 진행중

--- javaegoing/code/src/code/InheritanceApp.java
+++ javaegoing/code/src/code/InheritanceApp.java
@@ -4,6 +4,10 @@ class Cal{
 	public int sum(int v1, int v2) {
 		return v1+v2;
 	}
+	//Overloading
+	public int sum(int v1, int v2, int v3) {
+		return this.sum(v1, v2)+v3;
+	}
 }
 
 class Cal2{
@@ -17,7 +21,14 @@ class Cal2{
 
 
 class Cal3 extends Cal {
-	
+	public int minus(int v1, int v2) {
+		return v1-v2;
+	}
+	// Overriding
+	public int sum(int v1, int v2) {
+		System.out.println("Cal3!!");
+		return super.sum(v1,v2);
+	}
 }
 
 public class InheritanceApp {
@@ -28,6 +39,8 @@ public class InheritanceApp {
 		
 		Cal3 c3 = new Cal3();
 		System.out.println(c3.sum(2, 1));
+		System.out.println(c3.minus(2, 1));
+		System.out.println(c3.sum(2,1));
 
 	}
 
