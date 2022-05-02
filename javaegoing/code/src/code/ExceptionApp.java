4132ef269244f5fc711a47e5cf59c923aa0b2f2b
yh.thomas.lee <yh.thomas.lee@gmail.com>
2022-05-02 09:53:36 +0900

    exception using e.printStackTrace();

--- javaegoing/code/src/code/ExceptionApp.java
+++ javaegoing/code/src/code/ExceptionApp.java
@@ -12,13 +12,19 @@ public class ExceptionApp{
 			
 		System.out.println(2);
 		System.out.println(3);
-		System.out.println(scores[3]);
+		//System.out.println(scores[3]);
 		System.out.println(2/0);
 		System.out.println(4);
-		} catch (ArrayIndexOutOfBoundsException e) {
-			System.out.println("없는 값을 찾고 계십니다.");
-		} catch (ArithmeticException e) {
-			System.out.println("잘못된 계산이네요. ");
+		} catch(ArithmeticException e) {
+			System.out.println("계산이 잘못된 것 같아요"+e.getMessage());
+			e.printStackTrace();
+		
+		} catch(Exception e) {
+			System.out.println("오류가 발생했습니다.");
+//		} catch (ArrayIndexOutOfBoundsException e) {
+//			System.out.println("없는 값을 찾고 계십니다.");
+//		} catch (ArithmeticException e) {
+//			System.out.println("잘못된 계산이네요. ");
 		}
 		System.out.println(5);
 	}
