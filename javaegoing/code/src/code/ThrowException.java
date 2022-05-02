66ffec2d59a2927389f14190efa082cd4058e46a
yh.thomas.lee <yh.thomas.lee@gmail.com>
2022-05-02 19:20:24 +0900

    exception completed

---
+++ javaegoing/code/src/code/ThrowException.java
@@ -0,0 +1,14 @@
+package code;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class ThrowException {
+
+	public static void main(String[] args) throws IOException {
+		
+		FileWriter f = new FileWriter("./data.txt");
+
+	}
+
+}
